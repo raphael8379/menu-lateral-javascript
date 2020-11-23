1634a8340fc915356d6000a23fa380cbcdf9a86c
raphael anderson <raphael.8379@hotmail.com>
2020-11-23 11:09:03 -0300

    arquivo javascript

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+// Menu lateral
+
+document.getElementById("icone").onclick = function() {
+	document.getElementById("icone").style.transform = "translateX(-300px)";
+	document.getElementById("barra").style.transform = "translateX(-300px)";
+	
+	};
+	
+	document.getElementById("icone2").onclick = function() {
+		document.getElementById("icone").style.transform = "translateX(0)";
+	document.getElementById("barra").style.transform = "translateX(0)";
+	
+	};
+
+
+
